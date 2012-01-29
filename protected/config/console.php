ebd99206c0a0aee6c1bae310c426ae0a69cb139c
Hanra Gival <hanral2@gmail.com>
2012-01-29 14:20:48 +0200

    assets and config files

--- protected/config/console.php
+++ protected/config/console.php
@@ -8,17 +8,11 @@ return array(
 	// application components
 	'components'=>array(
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
 	),
 );
